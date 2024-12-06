37cded894a84cd7f90794800aabc686f85295e47
Theo Andreas <andreas.or80@gmail.com>
2024-12-06 11:04:25 +0700

    mengubah isi index.html

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello WOrld</title>
 </head>
+
 <body>
     <h1>Apa kabar</h>
-    <p>manajemen proyek</p>
-    <ul>
-        <l1>senin</li>
-        <li>selasa</li>
-        <li>Rabu</li>
-        <li>Kamis</li></li>
-            
+        <p>manajemen proyek</p>
+        <ul>
+            <l1>senin</li>
+                <li>selasa</li>
+                <li>Rabu</li>
+                <li>Kamis</li>
+                <li>Jumat</li>
 </body>
+
 </html>
